67c13ab01ddee8d566b9f69e506d5e49163410f1
Tithi Mehta <dharamehta012@gmail.com>
2024-02-17 23:00:18 +0530

    first commit dhara

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
-import { Button } from './components/ui/button'
+// import { Button } from './components/ui/button'
 import './App.css'
+import Navbar from './Mycomponents/Header'
+import { ThreeDCardDemo } from './Mycomponents/Card'
+import { LayoutGridDemo } from './Mycomponents/Grid'
+import Welcome from './Mycomponents/Welcome'
+import Footer from './Mycomponents/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button>
-        Submit
-      </Button>
+      <Navbar />
+      <Welcome/>
+      <LayoutGridDemo/>
+      <ThreeDCardDemo/>
+      <Footer/>
     </>
   )
 }
