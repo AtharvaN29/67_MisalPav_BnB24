ddcd7818b618f726fa2a457e4f115c5205416682
Ashish <ashishsatavase13@gmail.com>
2024-02-17 22:40:23 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import React from 'react';
 import { useState } from 'react'
 import { Button } from './components/ui/button'
+import AddProduct from './AddProduct'
 import './App.css'
-
+import Table from './Table';
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <Button>
-        Submit
-      </Button>
+    <div className='h-20'></div>
+    <div className='flex justify-end '><div className='w-1/2'><Table/></div></div>
+      
+     <AddProduct/>
+        
+ 
     </>
   )
 }
